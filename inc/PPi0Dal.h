96dda8e7b53beb5611a86f172b5b9adf854a4fd2
lena_heijken-PC <lenaheijken@gmail.com>
2017-05-10 15:32:33 +0200

    More playing in Pi0Dal (testing track classifications of signal).

--- inc/PPi0Dal.h
+++ inc/PPi0Dal.h
@@ -17,16 +17,22 @@ private:
     GH1*	time_cut_CB;
 
     TH1*    IM2g;
-    TH1*    IMeeg[2];
+    TH1*    IMeeg[3];
 
     TH2*    EdE_CBPID;
     TH2*    EdEdcTh_CBPID;
+    TH2*    EdE_TAVet;
+    TH2*    EdEdcTh_TAVet;
+
+    TH1*    CombChTra[1];
 
     TH1*	TaggerAccScal;
 
     //- Chosen reconstructed tracks
     std::vector<int> Track_neuCB;
     std::vector<int> Track_chaCB;
+    std::vector<int> Track_neuTA;
+    std::vector<int> Track_chaTA;
 
 protected:
     virtual Bool_t  Start();
--- src/PPi0Dal.cc
+++ src/PPi0Dal.cc
@@ -39,10 +39,9 @@ Bool_t	PPi0Dal::Start()
 void	PPi0Dal::ProcessEvent()
 {
     //- My own PID.. maybe
-    Track_neuCB.clear();
-    Track_neuCB.resize(0);
-    Track_chaCB.clear();
-    Track_chaCB.resize(0);
+    //-- First CB tracks
+    Track_neuCB.clear(); Track_neuCB.resize(0);
+    Track_chaCB.clear(); Track_chaCB.resize(0);
     for(Int_t i=0; i<GetTracks()->GetNTracks(); i++){
         if(GetTracks()->HasCB(i)){
             FillTime(*GetTracks(),i,time_CB);
@@ -62,20 +61,55 @@ void	PPi0Dal::ProcessEvent()
             }
         }
     }
+    //-- Then TAPS tracks
+    Track_neuTA.clear(); Track_neuTA.resize(0);
+    Track_chaTA.clear(); Track_chaTA.resize(0);
+    for(Int_t i=0; i<GetTracks()->GetNTracks(); i++){
+        if(GetTracks()->HasTAPS(i)){
+            Double_t Ecry=GetTracks()->GetClusterEnergy(i);
+            Double_t Evet=GetTracks()->GetVetoEnergy(i);
+            if(Ecry > 0.0 && !(Evet > 0.0)){
+                Track_neuTA.push_back(i);
+            }
+            if(Ecry > 0.0 && Evet > 0.0){
+                EdE_TAVet->Fill(Ecry,Evet);
+                Double_t thet=GetTracks()->GetTheta(i);
+                EdEdcTh_TAVet->Fill(Ecry*sin(thet),Evet*sin(thet));
+                Track_chaTA.push_back(i);
+            }
+        }
+    }
 
     //- Just checking stuff
-    Int_t ind1, ind2, ind3;
+    Int_t ind1, ind2, ind3, ind4;
+    Double_t PIDeCut = 1.05;
     if(Track_neuCB.size()==2){
-        ind1 = Track_neuCB[0];
-        ind2 = Track_neuCB[1];
+        ind1 = Track_neuCB[0]; ind2 = Track_neuCB[1];
         IM2g->Fill((GetTracks()->GetVector(ind1)+GetTracks()->GetVector(ind2)).M());
     }
     if(Track_neuCB.size()==1 && Track_chaCB.size()==2){
-        ind1 = Track_neuCB[0];
-        ind2 = Track_chaCB[0];
+        ind1 = Track_neuCB[0]; ind2 = Track_chaCB[0];
         ind3 = Track_chaCB[1];
-        IMeeg[1]->Fill()
+        IMeeg[0]->Fill((GetTracks()->GetVector(ind1)+GetTracks()->GetVector(ind2)+GetTracks()->GetVector(ind3)).M());
+        if(GetTracks()->GetVetoEnergy(ind2) < PIDeCut && GetTracks()->GetVetoEnergy(ind3) < PIDeCut){
+            IMeeg[1]->Fill((GetTracks()->GetVector(ind1)+GetTracks()->GetVector(ind2)+GetTracks()->GetVector(ind3)).M());
+        }
     }
+    if(Track_neuCB.size()==1 && Track_chaCB.size()==3){
+        ind1 = Track_neuCB[0]; ind2 = Track_chaCB[0];
+        ind3 = Track_chaCB[1]; ind4 = Track_chaCB[2];
+        Int_t inde[3]; Int_t Nre=0;
+        for(Int_t i=0; i<3; i++){
+            if(GetTracks()->GetVetoEnergy(Track_chaCB[i]) < PIDeCut){
+                inde[Nre]=Track_chaCB[i];
+                Nre++;
+            }
+        }
+        if(Nre == 2){
+            IMeeg[2]->Fill((GetTracks()->GetVector(ind1)+GetTracks()->GetVector(inde[0])+GetTracks()->GetVector(inde[1])).M());
+        }
+    }
+
 }
 
 void	PPi0Dal::ProcessScalerRead()
@@ -98,12 +132,18 @@ void    PPi0Dal::CreateHistograms()
 
     IM2g = new TH1F("IM2g","IM2g",1000,-0.5,999.5);
     IMeeg[0] = new TH1F("IMeeg1N2C","IMeeg1N2C",1000,-0.5,999.5);
-    IMeeg[1] = new TH1F("IMeeg1N3C","IMeeg1N3C",1000,-0.5,999.5);
-
+    IMeeg[1] = new TH1F("IMeeg1N2Ccut","IMeeg1N2Ccut",1000,-0.5,999.5);
+    IMeeg[2] = new TH1F("IMeeg1N3Ccut","IMeeg1N3Ccut",1000,-0.5,999.5);
 
     EdE_CBPID = new TH2F("EdE_CBPID","EdE_CBPID;E_{CB};dE_{PID}",200,0.,800.,200,0.,10.);
     EdEdcTh_CBPID = new TH2F("EdEdcTh_CBPID","EdEdcTh_CBPID;E_{CB};dE_{PID}*sin(#theta)",200,0.,800.,200,0.,10.);
+    EdE_TAVet = new TH2F("EdE_TAVet","EdE_TAVet;E_{TAPS};dE_{Veto}",200,0.,800.,200,0.,10.);
+    EdEdcTh_TAVet = new TH2F("EdEdcTh_TAVe","EdEdcTh_TAVe;E_{TAPS}*sin(#theta);dE_{Veto}*sin(#theta)",200,0.,800.,200,0.,10.);
 
+    CombChTra[0] = new TH1F("CombChTra_1CBneu","CombChTra_1CBneu",10,0.,10.);
+    CombChTra[0]->GetXaxis()->SetBinLabel(2,"3CB"); CombChTra[0]->GetXaxis()->SetBinLabel(3,"2CB(2e)1TA");
+    CombChTra[0]->GetXaxis()->SetBinLabel(4,"2CB(1e1p)1TA"); CombChTra[0]->GetXaxis()->SetBinLabel(5,"1CB(e)2TA");
+    CombChTra[0]->GetXaxis()->SetBinLabel(6,"1CB(p)2TA");
 
     TaggerAccScal = new TH1D("TaggerAccScal","TaggerAccScal",352,0,352);
 
